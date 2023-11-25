b601a9f88aced717824a3720accab0543119c2f9
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-11-25 14:50:07 +0000

    amend lisence headers

--- layouts/default/60_ansi_arrow_split_bs/default_60_ansi_arrow_split_bs/keymap.c
+++ layouts/default/60_ansi_arrow_split_bs/default_60_ansi_arrow_split_bs/keymap.c
@@ -1,4 +1,4 @@
-// Copyright 2020 QMK / Sendy YK
+// Copyright 2020 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later/
 
 #include QMK_KEYBOARD_H
--- layouts/default/60_ansi_arrow_tsangan/default_60_ansi_arrow_tsangan/keymap.c
+++ layouts/default/60_ansi_arrow_tsangan/default_60_ansi_arrow_tsangan/keymap.c
@@ -1,4 +1,4 @@
-// Copyright 2023 QMK / Sendy YK
+// Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
