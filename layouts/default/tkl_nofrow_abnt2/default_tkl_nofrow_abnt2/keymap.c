3dcb7a4b47332bd0acbbf2b5398a51d9d07072ba
Duncan Sutherland <dunk2k_2000@hotmail.com>
2024-04-04 15:08:04 +0100

    update keymap.c

--- layouts/default/tkl_nofrow_abnt2/default_tkl_nofrow_abnt2/keymap.c
+++ layouts/default/tkl_nofrow_abnt2/default_tkl_nofrow_abnt2/keymap.c
@@ -3,6 +3,8 @@
 
 #include QMK_KEYBOARD_H
 
+#include "keymap_brazilian_abnt2.h"
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
